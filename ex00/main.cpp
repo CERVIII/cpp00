e117557727397d2189fb7a7b86986096859c03ca
Pablo Cervilla miguel <pcervill@c1r5s6.42madrid.com>
2024-08-30 14:06:50 +0200

    ex00 completed

---
+++ ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 13:24:33 by pcervill          #+#    #+#             */
+/*   Updated: 2024/08/30 14:02:26 by pcervill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		i = 0;
+		while (argv[++i])
+		{
+			j = -1;
+			while (argv[i][++j])
+				std::cout << (char)toupper(argv[i][j]);
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
