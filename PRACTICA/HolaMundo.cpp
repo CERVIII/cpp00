f0395362b581b79747044a53a6e30c15aa4f1018
CERVIII <pablo.cervilla117@gmail.com>
2024-09-04 00:45:11 +0200

    Prueba push mac

--- PRACTICA/HolaMundo.cpp
+++ PRACTICA/HolaMundo.cpp
@@ -6,7 +6,7 @@
 /*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 00:20:36 by pcervill          #+#    #+#             */
-/*   Updated: 2024/09/04 00:20:59 by pcervill         ###   ########.fr       */
+/*   Updated: 2024/09/04 00:43:56 by pcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int main()
 {
+	string nombre;
+
+	nombre = "HOLA MUNDO";
 	std::cout << "Hola Mundo!" << std::endl;
+	std::cout << nombre;
 	return 0;
 }
