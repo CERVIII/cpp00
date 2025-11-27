148bfbfa8b7bb614ccd7cfe1695496192ce825d6
CERVIII <pablo.cervilla117@gmail.com>
2025-11-27 16:40:48 +0100

    ready to send

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 13:24:33 by pcervill          #+#    #+#             */
-/*   Updated: 2024/08/30 15:34:37 by pcervill         ###   ########.fr       */
+/*   Updated: 2025/11/27 16:35:41 by pcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ int	main(int argc, char *argv[])
 			j = -1;
 			while (argv[i][++j])
 				std::cout << (char)toupper(argv[i][j]);
+			if (argv[i + 1])
+				std::cout << " ";
 		}
 	}
 	std::cout << std::endl;
